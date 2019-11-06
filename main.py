c05fa9b57914ac03751e16eadfb6bad2e7ebc1a9
wendersonjps <wendersonjps@gmail.com>
2019-11-05 21:20:07 -0300

    Com comentários

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ b = 2
 
 soma = a + b
 
-if(soma > a):
-    print("Soma maior que A")
-elif(soma == a):
-    print("Soma igual a A")
-else:
-    print("Soma menor que A")
+if(soma > a): #Verificar se soma é maior que A
+    print("Soma maior que A") #Imprime o resultado
+elif(soma == a): #Verificar se soma é igual a A
+    print("Soma igual a A") #Imprime o resultado
+else: #Verificar se soma é menor que A
+    print("Soma menor que A") #Imprime o resultado
